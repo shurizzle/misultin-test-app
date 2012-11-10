8bd2856bcfe458765c5a66dbca0151062c4be474
shura <shura1991@gmail.com>
2012-11-10 13:33:25 +0100

    s/herml/erlydtl/

--- src/erweb_app.erl
+++ src/erweb_app.erl
@@ -1,9 +1,11 @@
 -module(erweb_app).
--export([start/1, stop/0]).
+-export([start/0, start/1, stop/0]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
+
+start() -> start(8000).
 
 start(Port) ->
   misultin:start_link([{port, Port}, {loop, fun(Req) -> handle_http(Req) end}]).
@@ -15,16 +17,20 @@ handle_http(Req) ->
   handle(Req:get(method), Req:resource([lowercase, urldecode]), Req).
 
 handle('GET', [], Req) ->
-  Req:ok([{"Content-Type", "text/plain"}], "Main home page.");
+  {ok, Res} = index_dtl:render(),
+  Req:ok([{"Content-Type", "text/html"}], Res);
 
 handle('GET', ["users"], Req) ->
-  Req:ok([{"Content-Type", "text/plain"}], "Main users root.");
+  {ok, Res} = users_dtl:render(),
+  Req:ok([{"Content-Type", "text/html"}], Res);
 
 handle('GET', ["users", UserName], Req) ->
-  Req:ok([{"Content-Type", "text/plain"}], "This is ~s's page.", [UserName]);
+  {ok, Res} = users_user_dtl:render([{"User", UserName}]),
+  Req:ok([{"Content-Type", "text/html"}], Res);
 
 handle('GET', ["users", UserName, "messages"], Req) ->
-  Req:ok([{"Content-Type", "text/plain"}], "this is ~s's messages page.", [UserName]);
+  {ok, Res} = users_user_messages_dtl:render([{"User", UserName}]),
+  Req:ok([{"Content-Type", "text/html"}], Res);
 
 handle(_, _, Req) ->
   Req:respond(404, [{"Content-Typpe", "text/plain"}], "Page not found.").
